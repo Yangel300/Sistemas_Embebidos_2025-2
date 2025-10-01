130ad0e4bb2000cec3a9ddec90412034d2ca2370
erquinteroc <147540973+erquinteroc@users.noreply.github.com>
2025-10-01 00:44:31 -0500

    Add files via upload

---
+++ Rules_sensors.pl
@@ -0,0 +1,51 @@
+go:-recoger_datos,
+    diagnostico(Resultado).
+
+recoger_datos :-
+    ask_number(temperatura),
+    ask_number(presion),
+    ask_number(oximetria).
+
+
+ask_number(Variable) :-
+    write('Ingrese '), write(Variable), write(': '),
+    read(Value), nl,
+    assertz(valor(Variable, Value)).
+
+
+%Hypothesis Identification Rules
+fiebre :-
+    valor(temperatura,T),
+    T>38.
+taquicardia :-
+    valor(presion,P),
+    P>120.
+
+hipoxia :-
+    valor(oximetria,O),
+    O<90.
+
+
+:- dynamic valor/2.
+% Reglas simples
+
+% --- Diagnóstico final ---
+%Predicado(Argumento)
+diagnostico(Resultado) :-
+    %findall/3 sirve para recolectar todas las soluciones de una consulta en una lista
+    %Mensaje es lo que quiero recolectar
+    findall(Mensaje,
+           %bloque de condiciones, fiebre, taquicardia, hipoxia
+           %El ; significa "o" en Prolog, asi que intenta todas esas posibilidades
+        ( (fiebre, Mensaje = 'El paciente tiene fiebre')
+        ; (taquicardia, Mensaje= 'El paciente tiene taquicardia')
+        ; (hipoxia, Mensaje = 'El paciente tiene hipoxia')), 
+        %ListaFinal es donde se guardan los resultado
+        ListaPre), 
+        sort(ListaPre, ListaFinal), %short ordena la lista y elimina duplicados
+            
+        %Condicional en prolog Si la lista está vacía ([]), entonces el resultado sera 'No se detectaron problemas' si no ; imprime la listaFinal
+   ( ListaFinal = [] -> 
+   Resultado = ['No se detectaron problemas'] 
+   ; Resultado = ListaFinal
+   ).
